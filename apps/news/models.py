8ea682f9c86a9ac818e0404235cf92aa523bcd88
Baki2009 <bakajsamatov@gmail.com>
2023-12-29 17:59:08 +0600

    commit

---
+++ apps/news/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django_resized.forms import ResizedImageField
+
+
+# Create your models here.
+class News(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Заголовок"
+    )
+    description = RichTextField(
+        verbose_name="Описание"
+    )
+    image = ResizedImageField(
+        force_format="WEBP",
+        upload_to="news_image/",
+        quality=100,
+        verbose_name="Фотография"
+    )
+    created = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+        null=True, blank=True
+    )
+    created_2 = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name="Новость"
+        verbose_name_plural="Новости"
